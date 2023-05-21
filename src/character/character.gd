0d7ac69132e2129cfacb92c055dbc1b4b0352061
myin <minyin142p@gmail.com>
2023-05-21 19:56:00 +0200

    setup actions

--- src/character/character.gd
+++ src/character/character.gd
@@ -1,14 +1,22 @@
 class_name Character
 extends CharacterBody2D
 
+signal action_cooldown()
+
 @export var speed := 50
 @export var anim: AnimationPlayer
 @export var sprite: Node2D
+@export var timer: Timer
 
 var dir = Vector2.UP
 
 func _ready():
 	dir = dir.rotated(TAU * randf())
+	motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
+	timer.timeout.connect(_on_action_timeout)
+
+func _on_action_timeout():
+	action_cooldown.emit()
 
 func _physics_process(delta):
 	velocity = dir * speed
@@ -21,3 +29,6 @@ func _physics_process(delta):
 		var collision = get_last_slide_collision()
 		if collision:
 			dir = dir.bounce(collision.get_normal())
+
+func do_action(board: Board):
+	pass
--- src/game.gd
+++ src/game.gd
@@ -2,11 +2,24 @@ extends Node2D
 
 @export var level: LevelResource
 @export var board: Board
+@export var action_timer: Timer
+
+var ready_characters: Array[Character] = []
 
 func _ready():
 	board.init_board(level.cards)
 	board.matched.connect(_spawn_card)
 	board.all_matched.connect(_win)
+	
+	action_timer.timeout.connect(_do_action)
+	action_timer.start()
+
+func _do_action():
+	var char = ready_characters.pick_random() as Character
+	ready_characters.erase(char)
+	char.action_cooldown.connect(func(): ready_characters.append(char))
+	await char.do_action(board)
+	action_timer.start()
 
 func _win():
 	print("You win")
@@ -15,6 +28,7 @@ func _spawn_card(card: CardResource, pos: Vector2):
 	var scene = card.character
 	if scene:
 		var char = scene.instantiate() as Node2D
+		ready_characters.append(char)
 		char.global_position = pos
 		get_tree().current_scene.add_child(char)
 	else:
