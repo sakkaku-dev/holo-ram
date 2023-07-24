381a65af703b4303b42421310b3d12958b6cf03a
myin <minyin142p@gmail.com>
2023-07-24 18:07:48 +0200

    add holo myth cover

--- src/GameManager.gd
+++ src/GameManager.gd
@@ -13,10 +13,14 @@ func _ready():
 
 func unlock_level():
 	var level = load(current_level_file)
-	unlocked_levels.append(Util.filename(current_level_file))
+	var level_name = Util.filename(current_level_file)
+	if not level_name in unlocked_levels:
+		unlocked_levels.append(level_name)
 
 	for card in level.cards:
-		unlocked_cards.append(Util.filename(card.resource_path))
+		var file_name = Util.filename(card.resource_path)
+		if not file_name in unlocked_cards:
+			unlocked_cards.append(file_name)
 	save_data()
 
 func save_data():
