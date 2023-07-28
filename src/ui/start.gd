7eab69041f20f96fa113db1d8076188120d7f3b2
myin <minyin142p@gmail.com>
2023-07-28 16:49:25 +0200

    refactor card view show

--- src/ui/start.gd
+++ src/ui/start.gd
@@ -1,14 +1,18 @@
-extends Menu
+extends Control
 
 @export var level_select: Control
 @export var card_view: Control
 
 func _ready():
-	change_menu(level_select)
+	card_view.position.x = -card_view.size.x
 
 func _on_card_view_pressed():
-	change_menu(card_view)
+	create_tween().tween_property(card_view, "position", Vector2(0, card_view.position.y), 1.0) \
+		.set_trans(Tween.TRANS_CUBIC) \
+		.set_ease(Tween.EASE_IN_OUT)
 
 
 func _on_close_card_view_pressed():
-	go_back()
+	create_tween().tween_property(card_view, "position", Vector2(-card_view.size.x, card_view.position.y), 1.0) \
+		.set_trans(Tween.TRANS_CUBIC) \
+		.set_ease(Tween.EASE_IN_OUT)
