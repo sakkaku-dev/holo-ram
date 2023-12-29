a4bd94698a7a3473d25d1f50f0c0b2e04485d35b
myin <minyin142p@gmail.com>
2023-12-29 17:12:55 +0100

    refactor card view

--- src/ui/CardView.gd
+++ src/ui/CardView.gd
@@ -3,7 +3,7 @@ extends Control
 const CARD_FOLDER = "res://src/cards/"
 
 @export var grid: GridContainer
-@export var texture: TextureRect
+@export var card_view_tex: TextureRect
 @export var name_label: Label
 @export var text_label: Label
 
@@ -29,21 +29,18 @@ func _ready():
 		tex.set_meta("type", type)
 		tex.stretch_mode = TextureRect.STRETCH_KEEP
 		tex.mouse_entered.connect(func(): _on_mouse_enter(tex, res, GameManager.is_card_type_unlocked(type)))
-		tex.mouse_exited.connect(func(): tex.z_index = 0)
 		grid.add_child(tex)
-		
-	
+
 func _on_mouse_enter(tex: TextureRect, card: CardResource, unlocked: bool):
-	tex.z_index = 10
 	_set_card_details(card, unlocked)
 
 func _set_card_details(card: CardResource, unlocked: bool):
 	if card:
 		name_label.text = tr(card.name) if unlocked else ""
 		text_label.text = tr(card.text) if unlocked else ""
-		texture.texture = card.illust
-		texture.modulate = Color.WHITE if unlocked else Color.BLACK
+		card_view_tex.texture = card.illust
+		card_view_tex.modulate = Color.WHITE if unlocked else Color.BLACK
 	else:
 		name_label.text = ""
 		text_label.text = ""
-		texture.texture = null
+		card_view_tex.texture = null
